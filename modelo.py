d163005bf24d50d443f1495038710f2959979cf3
alvaromlua <alvaromlua@gmail.com>
2024-04-26 20:34:23 -0500

    quitando modelo

--- modelo.py
+++ modelo.py
@@ -8,30 +8,44 @@ from sklearn import preprocessing
 global_learn_cls = None
 
 def load_model():
-    global global_learn_cls  # Indica que vamos a usar la variable global
-
-    df = pd.read_excel('Datos_noticias_fin_tot.xlsx')
     
+    df = pd.read_excel('Datos_noticias_fin_tot.xlsx')
+    # Instanciamos nuestro codificador de "labels"
     encoder = preprocessing.LabelEncoder()
+    # Y lo alimentamos con la información de las categorias
     encoder.fit(df["overall_sentiment"])
 
-    df_class = pd.DataFrame(data={
+    df_class = pd.DataFrame(data = {
         "text": df["Noticia_preprocesado"],
         "ref": encoder.transform(df["overall_sentiment"])
-    })
+        })
     
+    # Dividimos el conjunto de datos en 2: entrenamiento (90%) y pruebas (10%)
+    df_train, df_test = train_test_split(df_class, test_size=0.1, random_state=2023)
+    
+    # Cargar el tokenizador y el modelo preentrenado
     model_name = "mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    df_train, df_test = train_test_split(df_class, test_size=0.1, random_state=2023)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    
+    # Preparación del conjunto de datos de lenguaje
+    data_lm = TextDataLoaders.from_df(df_train, valid_pct=0.2, text_col="text", label_col="ref", is_lm=True, tokenizer=tokenizer)
+    # Entrenamiento del modelo de lenguaje
+    learn_lm = language_model_learner(data_lm, AWD_LSTM, drop_mult=0.7, metrics=accuracy)
+    suggested_lr = learn_lm.lr_find()
+    learn_lm.fit_one_cycle(1, lr_max=suggested_lr.valley)
+    learn_lm.unfreeze()
+    learn_lm.fit_one_cycle(5, lr_max=slice(suggested_lr.valley/(2.6**4), suggested_lr.valley))
+    learn_lm.save_encoder("ft_lm_distilroberta")
+
     data_cls = TextDataLoaders.from_df(df_train, valid_pct=0.2, text_col="text", label_col="ref", is_lm=False, tokenizer=tokenizer)
     learn_cls = text_classifier_learner(data_cls, AWD_LSTM, drop_mult=0.7, metrics=accuracy)
-    
     learn_cls = learn_cls.load_encoder("ft_lm_distilroberta")
     suggested_lr = learn_cls.lr_find()
     learn_cls.fit_one_cycle(1, lr_max=suggested_lr.valley)
     learn_cls.unfreeze()
     learn_cls.fit_one_cycle(5, lr_max=slice(suggested_lr.valley/(2.6**4), suggested_lr.valley))
-    
+
     # Guardamos el modelo en la variable global
     global_learn_cls = learn_cls
 
