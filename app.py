c289be72f3d1f85bf299ddc31430a732439ca40f
alvaromlua <alvaromlua@gmail.com>
2024-04-26 20:11:58 -0500

    modelo1

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import requests
 from datetime import datetime
 from flask import Flask, jsonify, request
-
+import modelo
 app = Flask(__name__)
 
+API_KEY = 'b05e46ab630a94da404aae7e5f08b7e1c347b8695bf1224a6b3d0a3617ff3930'
+URL = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
 
+_noticias = []
+
+##----------------------- OBTENER NOTICIAS DE LAS CRIPTOMONEDAS ELEGIDAS -----------------------##
 @app.route('/noticias', methods=['POST'])
 def get_crypto_news():
     API_KEY = 'b05e46ab630a94da404aae7e5f08b7e1c347b8695bf1224a6b3d0a3617ff3930'
@@ -47,6 +52,7 @@ def get_crypto_news():
         if news_info:
             fulfillment_text += f"Estas son las últimas noticias sobre tu criptomoneda\n--------------------------\n"
             for item in news_info:
+                _noticias.append(item['title'])
                 fulfillment_text += f"Titular de noticia : {item['title']}\nFecha: {item['date']}\nLink: {item['url']}\n--------------------------\n"
         else:
             fulfillment_text = "No hay noticias recientes de esta cryptomoneda"
@@ -68,7 +74,47 @@ def get_crypto_news():
         }
     })
 
+@app.route('/compararnoticias', methods=['POST'])
+def get_crypto_news_compare():
+    
+    fulfillment_text = ""
+    # Realizar la solicitud a la API
+    req = request.get_json()
+    query_result = req.get('queryResult', {})
+    parameters = query_result.get('parameters', {})
+    noticias = req.get('sessionInfo', {}).get('parameters', {}).get('cryptoparam', '').lower()
 
+    # Configuración de los parámetros para la solicitud a la API
+    params = {
+        'categories': noticias,
+    }
+
+    if noticias == "analiza esas noticias":
+        cont = 0
+        for _noticia in _noticias:
+            resultado_noticia = modelo.get_resultado_noticias(_noticia)
+            cont += 1 if resultado_noticia == "Positive" else -1
+        if cont > 0: 
+            fulfillment_text = 'Mis análisis sugieren una buena noticia para esta criptomoneda, puede ser una oportunidad valiosa para \n' 
+        else:
+            fulfillment_text = "Mis análisis indican que es mala noticia para esta criptomoneda, puede ser una oportunidad para comprar\n" 
+    else: 
+        resultado_noticia = modelo.get_resultado_noticias(noticias)
+        if resultado_noticia == "Positive":
+            fulfillment_text = f"Mis análisis sugieren una buena noticia para esta criptomoneda, puede ser una oportunidad valiosa para vender\n"
+        else:
+            fulfillment_text = f"Mis análisis indican que es mala noticia para esta criptomoneda, puede ser una oportunidad para comprar\n" 
+    
+    return jsonify({
+        "fulfillmentResponse": {
+            "messages": [{
+                "text": {
+                    "text": [fulfillment_text]
+                }
+            }]
+        }
+    })
 
 if __name__ == '__main__':
+    modelo.load_model()
     app.run(port=5000, debug=True)
---
+++ modelo.py
@@ -0,0 +1,52 @@
+from sklearn.model_selection import train_test_split
+from transformers import AutoTokenizer
+from fastai.text.all import *
+import pandas as pd
+from sklearn import preprocessing
+
+# Define la variable global learn_cls
+global_learn_cls = None
+
+def load_model():
+    global global_learn_cls  # Indica que vamos a usar la variable global
+
+    df = pd.read_excel('Datos_noticias_fin_tot.xlsx')
+    
+    encoder = preprocessing.LabelEncoder()
+    encoder.fit(df["overall_sentiment"])
+
+    df_class = pd.DataFrame(data={
+        "text": df["Noticia_preprocesado"],
+        "ref": encoder.transform(df["overall_sentiment"])
+    })
+    
+    model_name = "mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    df_train, df_test = train_test_split(df_class, test_size=0.1, random_state=2023)
+    data_cls = TextDataLoaders.from_df(df_train, valid_pct=0.2, text_col="text", label_col="ref", is_lm=False, tokenizer=tokenizer)
+    learn_cls = text_classifier_learner(data_cls, AWD_LSTM, drop_mult=0.7, metrics=accuracy)
+    
+    learn_cls = learn_cls.load_encoder("ft_lm_distilroberta")
+    suggested_lr = learn_cls.lr_find()
+    learn_cls.fit_one_cycle(1, lr_max=suggested_lr.valley)
+    learn_cls.unfreeze()
+    learn_cls.fit_one_cycle(5, lr_max=slice(suggested_lr.valley/(2.6**4), suggested_lr.valley))
+    
+    # Guardamos el modelo en la variable global
+    global_learn_cls = learn_cls
+
+
+def get_resultado_noticias(texto_ejemplo):
+    
+    if global_learn_cls is None:
+        raise ValueError("El modelo no ha sido cargado. Por favor, ejecuta primero load_model().")
+    
+    clase_a_etiqueta = {0: "Negative", 1: "Positive"}
+    prediccion = global_learn_cls.predict(texto_ejemplo)
+    clase_predicha = clase_a_etiqueta[int(prediccion[0])]
+    
+    return clase_predicha
+
+
+if __name__ == '__main__':
+    load_model()
